76af7b2c9ed176696960fe6c54a4220040c774ba
AdriSvm <adria.svm@gmail.com>
2023-05-01 18:51:49 +0200

    Code commented, Final implementation

--- main.py
+++ main.py
@@ -3,10 +3,25 @@ import numpy as np
 from matplotlib import pyplot as plt
 from model import Capa
 
-np.set_printoptions(threshold=600)
 
 class OP_Problema:
     def __init__(self,nrows:int=None,ncols:int=None,nobstacles:int=0,pos_obstacles:list=[]):
+        """
+        Object Programming oriented implementation of a basic avalanche.
+        We create a main layer where the avalanche initiates in a fixed or random point and where you can
+        see the evolution of it.
+        We made two similiar implementations:
+            -First one incorporates a layer of obstacles which can be natural stuff like rocks or big trees,
+             but also made to implement avalanche protections. We consider that these obstacles stop completely
+             the avalanche. This one can be called through .evolve() method.
+            -In the second implementation we add a third layer of snow. This layer it's made of floats which
+             reflects how much snow there's in the mountain. As we're modeling a constant slope, it's made like
+             a gradient, in the top we have a lot of snow and very little in the bottom. This implementation
+             can be called through the evolve_prop method. The snow gains power with the snow it encounters
+             but, it also divides its power proportionally to the three cels below.
+        Numpy is compulsory and a random seed.
+        The layers are created through a model called Capa which retains all the information about them
+        """
         self._nrows = nrows if nrows else 50
         self._ncols = ncols if ncols else 50
         random.seed(2224)
@@ -15,18 +30,16 @@ class OP_Problema:
         # capa obstacles
         self._cobstacles = Capa(nrow=nrows,ncol=ncols,init_value=0)
         self._cneu = self.gen_snow_layer()
-        self._capes = []
         if len(pos_obstacles) == nobstacles:
             for i in pos_obstacles:
                 self._cobstacles.gen_layer_values(i[0],i[1],i[2],i[2],1)
         else:
             self._cobstacles.gen_random_layers(nobstacles,value=1)
 
-    def afegir_capa(self,init_value):
-
-        c = Capa(nrow=self._nrows,ncol=self._ncols,init_value=init_value)
-
     def evolve(self) -> np.array:
+        """
+        Main method which creates the first implementation of the avalanche, the start is randomly created
+        """
         els = []
         els.append((random.randint(0,self._matriu.shape()[0]-1),random.randint(0,self._matriu.shape()[1]-1)))
         print(els)
@@ -34,7 +47,7 @@ class OP_Problema:
             print(els)
             nucli = els.pop()
             if self._matriu.Capa[nucli[0]][nucli[1]] == 0:
-                for i,val in enumerate(self.vicinity30(self._matriu,nucli[0],nucli[1],self._cobstacles)):
+                for i,val in enumerate(self.vicinity(self._matriu,nucli[0],nucli[1],self._cobstacles)):
                     self._matriu.change_value(nrow=nucli[0],ncol=nucli[1],value=1)
                     if val == 0:
                         els.append((nucli[0]+1,nucli[1]+i-1))
@@ -42,27 +55,30 @@ class OP_Problema:
         return self._matriu.Capa, self._cobstacles.Capa
 
     def evolve_prop(self,init_row:int,init_col:int) -> np.array:
+        """
+        Second implementation of the avalanche, here we consider the third layer of snow
+        init_row and init_col precises where the avalanche starts.
+        """
         self._matriu.change_value(init_row,init_col,self._cneu.Capa[init_row,init_col])
         print(self._matriu.Capa[init_row,init_col])
         for i in range(init_row,self._nrows):
             for x,val in enumerate(self._matriu.Capa[i]):
                 print(val) if val != 0 else None
-                if val > 0.05:
-                    veins = self.vicinity30(self._matriu,i,x,self._cobstacles)
-                    veins_neu = self.get_snow_neighbours(i,x) # Crear nova capa de quantitat de neu per cada casella
-                    print("veins_init",veins)
+                if val > 0.4: #IMPORTANT LINE, Changing the 0.4 value, we can adjust how much snow we consider to still evolve the avalanche
+                    veins = self.vicinity(self._matriu,i,x,self._cobstacles)
+                    veins_neu = self.get_snow_neighbours(i,x) # Nova capa de quantitat de neu per cada casella
                     num_dif_de_zero = sum(1 for elem in veins if elem == 0)
                     veins = tuple(float((val / num_dif_de_zero) + veins_neu[o]) if veins[o] == 0 else 0 for o in range(len(veins)))
-                    print("veins",veins, "caselles", i+1,i+1,x-1,x-2)
                     for m,val in enumerate(veins):
                         if val != 0:
                             self._matriu.gen_one_value(i+1,x+m-1,val)
-                    #self._matriu.gen_values(i+1,i+1,x-1,x+2,veins)
-                    #self._matriu.Capa[i+1,x-1:x+2] = veins
 
         return self._matriu.Capa,self._cobstacles.Capa, self._cneu.Capa
 
     def get_snow_neighbours(self,nrow,ncol):
+        """
+        Vicinity function of the snow layer
+        """
         if nrow == self._matriu.shape()[0] - 1:
             return (0,0,0)
         elif ncol == 0:
@@ -71,23 +87,25 @@ class OP_Problema:
             return (self._cneu.Capa[nrow + 1, ncol - 1], self._cneu.Capa[nrow + 1, ncol], 0)
         else:
             return (self._cneu.Capa[nrow + 1, ncol - 1], self._cneu.Capa[nrow + 1, ncol],self._cneu.Capa[nrow+1,ncol+1])
+
     def gen_snow_layer(self):
-        inc = 0.6
+        """
+        Method which creates the snow layer with the gradient method, we consider that the start of the layer is the
+        top of the mountain, so there's more snow and as we go down the mountain, the quantity of snow decreases.
+        """
         snow = np.zeros((int(self._nrows),int(self._ncols)))
         for i in range(self._nrows):
             alt_rel = int(self._nrows*0.95) -i
-            for j in range(self._ncols):
-                distancia_al_centro = abs(j - (self._ncols - 1) / 2)
-                ancho_mitad = self._ncols / 2
-                factor_anchura = np.exp(-distancia_al_centro ** 2 / (2 * (ancho_mitad * inc) ** 2))
-                nieve = alt_rel * factor_anchura
-                snow[i, j] = max(nieve, 0)
-        return Capa(0,0,0,snow)
+            nieve = alt_rel / (self._nrows - 1)  # calcular la cantidad de nieve en función de la altura
+            snow[i, :] = nieve  # asignar la cantidad de nieve a todas las columnas en la fila i
+        return Capa(0, 0, 0, snow)
 
 
     @staticmethod
-    def vicinity30(matriu,fila,columna, matriu_obstacles):
-        print(fila,columna)
+    def vicinity(matriu,fila,columna, matriu_obstacles):
+        """
+        The main vicinity function of the main layer
+        """
         # 0: pot propagar, 1: no pot propagar
         if fila == matriu.shape()[0]-1:
             return (1,1,1)
@@ -118,34 +136,37 @@ class OP_Problema:
             return (esq, mig, dret)
 
 
+#Creation of the main object for the modeling of the avalanche
+#Here we consider a layer of 220*220 and 10 obstacles, we can also precise the position of the obstacles
 proba = OP_Problema(220,220,10)
-res,obst,neu = proba.evolve_prop(40,120)
-print(res,obst,sep='\n\n\n')
 
+#You can call the first implementation with the next line:
+#res,obst = proba.evolve()
+
+# And the second implementation is called through the next line, we specify the position of the start of the avalanche
+res,obst,neu = proba.evolve_prop(1,100)
+
+#Then we plot the simulation results
 fig, ax = plt.subplots()
 
-# Mostramos la matriz como una imagen en blanco y negro
+
 ax.imshow(res, cmap='viridis')
 
 
-# Mostramos el gráfico
 plt.show()
 
 fig, ax = plt.subplots()
 
-# Mostramos la matriz como una imagen en blanco y negro
 ax.imshow(obst, cmap='binary')
 
 
-# Mostramos el gráfico
 plt.show()
 
 
 fig, ax = plt.subplots()
 
-# Mostramos la matriz como una imagen en blanco y negro
+
 ax.imshow(neu, cmap='viridis')
 
 
-# Mostramos el gráfico
 plt.show()
--- model.py
+++ model.py
@@ -1,16 +1,26 @@
 import numpy as np, random
 class Capa():
     def __init__(self, nrow:int,ncol:int,init_value:[int,float,str,bool],matrix=None):
+        """
+        Layer implementation in an Object Programming oriented way, to make easier the creation of new layers and its
+        modifications.
+        The layer can be created through 2 methods:
+            -Specificating the nrows, ncols, and the init value of the layer or through a matrix created before
+        Then you can see a few method to make easier the modifications of the layer
+        """
         if matrix is not None:
             self.Capa=matrix
             self.nrows = self.shape()[0]
             self.ncols = self.shape()[0]
         else:
-            self.Capa = np.array([[init_value for _ in range(ncol)] for _ in range(nrow)])
+            self.Capa = np.array([[init_value for _ in range(ncol)] for _ in range(nrow)]) # Main object with the layer
             self.nrows = nrow
             self.ncols = ncol
 
     def change_value(self,nrow:int,ncol:int,value):
+        """
+        Change the value of a specific [nrow,ncol] cel with a value
+        """
         try:
             self.Capa[nrow,ncol] = value
             return True
@@ -18,6 +28,9 @@ class Capa():
             return False
 
     def gen_random_samples(self,n:int,value):
+        """
+        Generate random cels of the layer, to set random positions with a specific value
+        """
         for _ in range(n):
             a = random.randint(0, self.nrows - 1)
             b = random.randint(a, self.ncols - 1)
@@ -25,6 +38,9 @@ class Capa():
         return self.Capa
 
     def gen_random_layers(self,n,value):
+        """
+        Generate n random layers modified to a value. We use this to create the obstacles
+        """
         for _ in range(n):
             a = random.randint(0, self.shape()[1] - 1)
             b = random.randint(a, self.shape()[1] - 1)
@@ -32,15 +48,29 @@ class Capa():
             pared = (random.randint(0, self.shape()[0] - 1), random.randrange(0, self.shape()[1] - 1))
             self.Capa[pared[0]][a:b] = value
     def gen_one_value(self,nrow:int,ncol:int,value):
+        """
+        Set one specific value to a specific [nrow,ncol] cel
+        """
         self.Capa[nrow,ncol] = value
 
     def gen_values(self,nrow_init:int,nrow_fin:int,ncol_init:int,ncol_fin:int,values):
+        """
+        Set a specific range of cels to a specific value (values)
+        """
         self.Capa[nrow_init:nrow_fin+1,ncol_init:ncol_fin] = values
 
     def gen_layer_values(self,colinit:int,colfin:int,rowinit:int,rowfin:int,value):
+        """
+        Sane as gen_values, IDK why I reimplemented it
+        I think at first I created one method for specific cel changing values and for a range of values and then
+        I realised that it can be made with only one method.
+        """
         self.Capa[rowinit:rowfin+1,colinit:colfin] = value
 
     def shape(self):
+        """
+        Easy use of .shape numpy.array method
+        """
         sh = self.Capa.shape
         try:
             a = sh[1]
